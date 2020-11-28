8272e97987a52dae3f85ddcaca6a830ec4ccc093
unimatrix2 <dnny.nis@gmail.com>
2020-11-28 16:00:06 -0300

    added loginmodal

---
+++ src/components/Auth/LoginModal.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './LoginModal.css';
+
+const LoginModal = ({active}) => {
+    return (
+        
+    )
+}
+
+export default LoginModal;
