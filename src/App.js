90077cc42d7052201b6fcd4eb7c27cbf2fc31476
unimatrix2 <dnny.nis@gmail.com>
2020-11-28 00:15:27 -0300

    added navbar and basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import About from './components/About/About';
+import 'bulma/css/bulma.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 function App() {
     return (
         <div className="App">
-            
+            <Navbar />
+
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/sobre" render={() => <About isFaq={false} />} />
+                <Route exact path="/faq" render={() => <About isFaq={true} />} />
+            </Switch>
         </div>
     );
 }
