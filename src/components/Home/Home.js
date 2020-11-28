a95b3d4a3ffa349cad6600ffbb7777baa4d28d2f
unimatrix2 <dnny.nis@gmail.com>
2020-11-28 00:42:52 -0300

    fixed class names and added joining options

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,13 +5,13 @@ import './Home.css';
 const Home = () => {
     return (
             <main className="main-home-content">
-                <div class="tile is-parent tile-margin">
+                <div class="tile is-parent home-tile">
                     <article class="tile is-child tile-cambridge-blue">
                         <p class="title">Meu Político</p>
                         <p class="subtitle">O termômetro da honestidade</p>
                     </article>
                 </div>
-                <div class="tile is-parent tile-margin pitch">
+                <div class="tile is-parent home-tile pitch">
                     <article class="tile is-child tile-cambridge-blue pitch-text">
                         <p class="subtitle">Se ao menos a gente pudesse contar as mentiras desses políticos... poderíamos 
                         fazer com que  nos contem novas mentiras! Ou de repente… serem honestos, mas essa última proposta 
@@ -21,8 +21,16 @@ const Home = () => {
                         <Link to="/sobre" className="button button-light-blue-home">Conheça o Projeto!</Link>
                     </article>
                 </div>
-                <div className="register-group">
-
+                <div class="tile is-parent home-tile">
+                    <article class="tile is-child tile-cambridge-blue">
+                        <p class="title">Participe!</p>
+                        <Link className="button button-ultra-red mr-6">
+                            Seja um voluntário
+                        </Link>
+                        <Link className="button button-light-blue-home" >
+                            Crie uma conta
+                        </Link>
+                    </article>
                 </div>
             </main>
     )
