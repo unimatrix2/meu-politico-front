01670104dd4137337d454d45622b4f34041ce99b
unimatrix2 <dnny.nis@gmail.com>
2020-12-01 00:26:46 -0300

    moved component to own folder

--- src/components/Auth/Signup/SignupModal.js
+++ src/components/Auth/Signup/SignupModal.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import { Modal, Button, Form, Col } from 'react-bootstrap';
 import { Formik } from 'formik';
 import * as yup from 'yup';
