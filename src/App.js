16318d23443fc33542f48a5ae93fd177070e3651
unimatrix2 <dnny.nis@gmail.com>
2020-12-05 18:23:27 -0300

    implemented api service

--- src/App.js
+++ src/App.js
@@ -4,36 +4,27 @@ import NavigationBar from './components/Navbar/Navbar';
 import About from './components/About/About';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { get, remove } from './utils/localStorage.utils';
-import axios from 'axios';
+import api from './services/api.service'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     const [showSignup, setShowSignup] = useState(false);
     const [showLogin, setShowLogin] = useState(false);
-    const [isAuthed, setIsAuthed] = useState(() => {
-        const token = get();
-        if (token) { return true }
-        return false;
-    });
+    const [isAuthed, setIsAuthed] = useState(get() ? true : false);
     const [currentUser, setCurrentUser] = useState({});
     useEffect(() => {
-        const token = get();
-        if (token) {
-            axios.get("http://localhost:5000/api/usuario/token", {
-                headers: { Authorization: token.token }
-            })
-            .then(data => setCurrentUser({
-                firstName: data.data.firstName,
-                lastName: data.data.lastName,
-                email: data.data.email,
-                cpf: data.data.cpf,
-                imageURL: data.data.imageURL,
-                role: data.data.role,
-            }))
-            .catch(err => {
-                remove();
-                setIsAuthed(false);
-            })
+        if (isAuthed) {
+            api.get(`${process.env.REACT_APP_API_BASE_URL}/usuario/token`)
+                .then(data => setCurrentUser({
+                        firstName: data.data.firstName,
+                        lastName: data.data.lastName,
+                        email: data.data.email,
+                        cpf: data.data.cpf,
+                        imageURL: data.data.imageURL,
+                        role: data.data.role,
+                    })
+                )
+                .catch(() => setIsAuthed(false));
         }
     }, [isAuthed])
     return (
@@ -49,7 +40,7 @@ function App() {
             />
 
             <Switch>
-                <Route exact path="/" render={() => <Home showSignup={showSignup} setShowSignup={setShowSignup} />} />
+                <Route exact path="/" render={() => <Home showSignup={showSignup} setShowSignup={setShowSignup} authState={isAuthed} />} />
                 <Route exact path="/sobre" render={() => <About isFaq={false} />} />
                 <Route exact path="/faq" render={() => <About isFaq={true} />} />
             </Switch>
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-import { reqInterceptor, errorInterceptor } from './interceptors/api.interceptors';
+import { reqInterceptor, errorInterceptor } from './interceptors/api.interceptor';
 
 const api = axios.create({});
 
 api.interceptors.request.use(reqInterceptor);
 
 api.interceptors.response.use(res => res, errorInterceptor);
+
+export default api;
