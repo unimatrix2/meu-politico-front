faea2137eae121e96877a67b68566d1d85508d0c
unimatrix2 <dnny.nis@gmail.com>
2020-12-09 22:03:21 -0300

    added create politico modal

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Button, Table } from 'react-bootstrap';
+import CreatePolitico from '../Forms/CreatePolítico';
 import './Dashboard.css';
 
 const Dashboard = ({userData}) => {
+    const [showCreatePolitico, setShowCreatePolitico] = useState(false);
     return (
         <>
             <Container fluid className="d-flex dashboard-container justify-content-center">
@@ -12,7 +14,13 @@ const Dashboard = ({userData}) => {
                         <Container className="dashboard-table-container">
 
                         </Container>
-                        <Button size="lg" className="align-self-center mt-4">Criar Político</Button>
+                        <Button
+                            size="lg"
+                            className="align-self-center mt-4"
+                            onClick={() => setShowCreatePolitico(true)}
+                        >
+                            Criar Político
+                        </Button>
                     </Col>
                     <Col lg={4} className="d-flex flex-column">
                         <h2>Minhas Notícias</h2>
@@ -30,6 +38,7 @@ const Dashboard = ({userData}) => {
                     </Col>
                 </Row>
             </Container>
+            <CreatePolitico show={showCreatePolitico} onHide={() => setShowCreatePolitico(false)} />
         </>
     )
 }
